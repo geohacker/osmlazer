d3400e32f86d79d2b204a16c1b777aa7a886b9f5
Sajjad Anwar <me@sajjad.in>
2016-11-10 18:41:13 +0530

    extract turn-restrictions

--- index.js
+++ index.js
@@ -8,43 +8,14 @@ var reader = new osmium.Reader(file);
 var location_handler = new osmium.LocationHandler();
 var stream = new osmium.Stream(new osmium.Reader(file, location_handler));
 
-if (argv.filter && fs.existsSync(argv.filter)) {
-    argv.filter = JSON.parse(fs.readFileSync(argv.filter));
-} else {
-    argv.filter = false;
-    process.exit();
-}
-
-var filter = ff(argv.filter);
-
 stream.on('data', function (data) {
     var f;
-    try {
-        f = getFeature(data);
-        if (f && filter(f)) {
-            var fc = {
-                'type': 'FeatureCollection',
-                'features': [f]
-            };
-            console.log(JSON.stringify(fc));
-        }
-    } catch (e) {
-        return;
-    }
-
+    var tags = data.tags();
+    if (tags.hasOwnProperty('type') && tags.type === 'restriction') {
+        console.log(data);
+    };
 });
 
 stream.on('end', function() {
     process.stderr.write('done');
 });
-
-function getFeature(d) {
-    var feature = {
-        'type': 'Feature',
-        'geometry': d.geojson(),
-        'properties': d.tags()
-    };
-
-    return feature;
-
-}
