63d2c90c431d22e9216e26927f71287aeb9e9d11
Ramya <ramya@mapbox.com>
2017-01-06 23:35:29 +0530

    remove mbx count + display JSON

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ var osmium = require('osmium');
 var argv = require('minimist')(process.argv.slice(2));
 var ff = require('feature-filter');
 var fs = require('fs');
-var team = require('mapbox-data-team').getUsernames();
+
 
 var file = new osmium.File(argv.file);
 var reader = new osmium.Reader(file);
 var stream = new osmium.Stream(new osmium.Reader(file));
 
-var mapboxCount = 0;
-var otherCount = 0;
 var countJson = {};
 
 stream.on('data', function (data) {
@@ -22,15 +20,11 @@ stream.on('data', function (data) {
           countJson['restriction'] = 1;
         }
 
-        if (!(team.indexOf(data.user) < 0 )) {
-           mapboxCount = mapboxCount + 1;
-       } else {
-        otherCount = otherCount + 1
-       }
+        
    }
 });
 
 stream.on('end', function() {
-    process.stderr.write(countJson + '\n');
+    process.stderr.write(JSON.stringify(countJson) + '\n');
     
 });
