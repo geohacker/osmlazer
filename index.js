cc4233f2bfab9d65735cc03d32f4fe7add9d15bc
Ramya <ramya@mapbox.com>
2017-01-06 23:20:43 +0530

    new branch + Json object for count

--- index.js
+++ index.js
@@ -2,49 +2,35 @@ var osmium = require('osmium');
 var argv = require('minimist')(process.argv.slice(2));
 var ff = require('feature-filter');
 var fs = require('fs');
+var team = require('mapbox-data-team').getUsernames();
 
 var file = new osmium.File(argv.file);
 var reader = new osmium.Reader(file);
-var location_handler = new osmium.LocationHandler();
-var stream = new osmium.Stream(new osmium.Reader(file, location_handler));
+var stream = new osmium.Stream(new osmium.Reader(file));
 
-if (argv.filter && fs.existsSync(argv.filter)) {
-    argv.filter = JSON.parse(fs.readFileSync(argv.filter));
-} else {
-    argv.filter = false;
-    process.exit();
-}
-
-var filter = ff(argv.filter);
+var mapboxCount = 0;
+var otherCount = 0;
+var countJson = {};
 
 stream.on('data', function (data) {
     var f;
-    try {
-        f = getFeature(data);
-        if (f && filter(f)) {
-            var fc = {
-                'type': 'FeatureCollection',
-                'features': [f]
-            };
-            console.log(JSON.stringify(fc));
+    var tags = data.tags();
+    if (tags.hasOwnProperty('type') && tags.type === 'restriction') {
+        if countJson.hasOwnProperty('restriction'){
+          countJson['restriction'] = countJson['restriction']+1;
+        } else {
+          countJson['restriction'] = 1;
         }
-    } catch (e) {
-        return;
-    }
 
+        if (!(team.indexOf(data.user) < 0 )) {
+           mapboxCount = mapboxCount + 1;
+       } else {
+        otherCount = otherCount + 1
+       }
+   }
 });
 
 stream.on('end', function() {
-    process.stderr.write('done');
+    process.stderr.write(countJson + '\n');
+    
 });
-
-function getFeature(d) {
-    var feature = {
-        'type': 'Feature',
-        'geometry': d.geojson(),
-        'properties': d.tags()
-    };
-
-    return feature;
-
-}
