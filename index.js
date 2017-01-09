c2859742aae403257e419ec31c819ba995636f08
Ramya <ramya@mapbox.com>
2017-01-09 20:17:31 +0530

    wiki atg count for countries mapbox/communications/#1282

--- index.js
+++ index.js
@@ -7,30 +7,54 @@ var file = new osmium.File(argv.file);
 var reader = new osmium.Reader(file);
 var location_handler = new osmium.LocationHandler("dense_mmap_array");
 var stream = new osmium.Stream(new osmium.Reader(file, location_handler));
+var count, wikidata_count, both_count, just_wikidata_count, just_wikipedia_count, notag_count = 0;
+
 
 stream.on('data', function (data) {
-    var f;
-    var tags = data.tags();
-    if (tags.hasOwnProperty('wikidata')) {
+  var f;
+  var tags = data.tags();
+  if (tags.hasOwnProperty('place') && tags.place ==='country' && data.type === 'node') {
+    count ++;
+    if (tags.hasOwnProperty('wikidata')){
       try {
         f = getFeature(data);
         console.log(JSON.stringify(f));
+        wikidata_count++;
+        
       } catch (e) {
         return;
       }
+    } 
+    if(tags.hasOwnProperty('wikidata') && tags.hasOwnProperty('wikipedia')){
+      both_count++;
+
     }
+    if(tags.hasOwnProperty('wikidata') && !tags.hasOwnProperty('wikipedia')){
+      just_wikidata_count++;
+
+    }
+    if(!tags.hasOwnProperty('wikidata') && tags.hasOwnProperty('wikipedia')){
+      just_wikipedia_count++;
+
+    }
+    if(!tags.hasOwnProperty('wikidata') && !tags.hasOwnProperty('wikipedia')){
+      notag_count++;
+
+    }
+  }
 });
 
 stream.on('end', function() {
-  process.exit(0);
+  process.stderr.write('Total number of countries: '+JSON.stringify(count) + '\n');
+ 
 });
 
 function getFeature(d) {
-    var feature = {
-        'type': 'Feature',
-        'osm_id':d.id,
-        'geometry': d.geojson(),
-        'properties': d.tags()
-    };
-    return feature;
+  var feature = {
+    'type': 'Feature',
+    'osm_id':d.id,
+    'geometry': d.geojson(),
+    'properties': d.tags()
+  };
+  return feature;
 }
