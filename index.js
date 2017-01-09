7ab323b191804de55c735954709ed7867e0ad73c
Ramya <ramya@mapbox.com>
2017-01-09 20:56:34 +0530

    var cleanup + count o/p

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var file = new osmium.File(argv.file);
 var reader = new osmium.Reader(file);
 var location_handler = new osmium.LocationHandler("dense_mmap_array");
 var stream = new osmium.Stream(new osmium.Reader(file, location_handler));
-var count, wikidata_count, both_count, just_wikidata_count, just_wikipedia_count, notag_count = 0;
+var count =0, wikidata_count=0, both_count=0, just_wikidata_count=0, just_wikipedia_count=0, notag_count = 0;
 
 
 stream.on('data', function (data) {
   var f;
   var tags = data.tags();
-  if (tags.hasOwnProperty('place') && tags.place ==='country' && data.type === 'node') {
-    count ++;
+  if (tags.hasOwnProperty('place') && tags.place === 'neighbourhood'  && data.type === 'node') {
+    count++;
     if (tags.hasOwnProperty('wikidata')){
       try {
         f = getFeature(data);
@@ -46,7 +46,11 @@ stream.on('data', function (data) {
 
 stream.on('end', function() {
   process.stderr.write('Total number of countries: '+JSON.stringify(count) + '\n');
- 
+  process.stderr.write('Wikidata: '+ JSON.stringify(wikidata_count) + '\n');
+  process.stderr.write('Only wikidata: '+JSON.stringify(just_wikidata_count)+'\n');
+  process.stderr.write('Only wikipedia: '+JSON.stringify(just_wikipedia_count)+'\n');
+  process.stderr.write('Both: '+JSON.stringify(both_count)+'\n');
+  process.stderr.write('None: '+JSON.stringify(notag_count)+'\n');
 });
 
 function getFeature(d) {
